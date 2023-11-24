64b0fc47d60b4f9de9393d9c3452285064cd0508
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-11-24 17:51:06 +0900

    MT4_01_04

--- Project/Scene/TitleScene.cpp
+++ Project/Scene/TitleScene.cpp
@@ -1,4 +1,5 @@
 #include "TitleScene.h"
+#include "Engine/Math/MathFunction.h"
 
 TitleScene::~TitleScene() {
 
@@ -10,7 +11,25 @@ void TitleScene::Initialize() {
 }
 
 void TitleScene::Update() {
+	Quaternion rotation = MakeRotateAxisAngleQuaternion(Normalize(Vector3{ 1.0f,0.4f,-0.2f }), 0.45f);
+	Vector3 pointY = { 2.1f,-0.9f,1.3f };
+	Matrix4x4 rotateMatrix = MakeRotateMatrix(rotation);
+	Vector3 rotateByQuaternion = RotateVector(pointY, rotation);
+	Vector3 rotateByMatrix = Transform(pointY, rotateMatrix);
 
+	ImGui::Begin("TitleScene");
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f : rotation",
+		rotation.x, rotation.y, rotation.z, rotation.w);
+	ImGui::Text("rotateMatrix\n%1.3f %1.3f %1.3f %1.3f\n%1.3f %1.3f %1.3f %1.3f\n%1.3f %1.3f %1.3f %1.3f\n%1.3f %1.3f %1.3f %1.3f",
+		rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
+		rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
+		rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+		rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+	ImGui::Text("%1.2f %1.2f %1.2f : rotateByQuaternion",
+		rotateByQuaternion.x, rotateByQuaternion.y, rotateByQuaternion.z);
+	ImGui::Text("%1.2f %1.2f %1.2f : rotateByMatrix",
+		rotateByMatrix.x, rotateByMatrix.y, rotateByMatrix.z);
+	ImGui::End();
 }
 
 void TitleScene::Draw() {
