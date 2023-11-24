e65be199c7cd03aa34c91c660c7fc7b0182d57ad
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-11-24 17:50:04 +0900

    MT4_01_03

--- Project/Scene/TitleScene.cpp
+++ Project/Scene/TitleScene.cpp
@@ -1,4 +1,5 @@
 #include "TitleScene.h"
+#include "Engine/Math/MathFunction.h"
 
 TitleScene::~TitleScene() {
 
@@ -10,7 +11,31 @@ void TitleScene::Initialize() {
 }
 
 void TitleScene::Update() {
+	Quaternion q1 = { 2.0f,3.0f,4.0f,1.0f };
+	Quaternion q2 = { 1.0f,3.0f,5.0f,2.0f };
+	Quaternion identity = IdentityQuaternion();
+	Quaternion conj = Conjugate(q1);
+	Quaternion inv = Inverse(q1);
+	Quaternion normal = Normalize(q1);
+	Quaternion mul1 = Multiply(q1, q2);
+	Quaternion mul2 = Multiply(q2, q1);
+	float norm = Norm(q1);
 
+	ImGui::Begin("TitleScene");
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f     : Identity",
+		identity.x, identity.y, identity.z, identity.w);
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f  : Conjugate",
+		conj.x, conj.y, conj.z, conj.w);
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f  : Inverse",
+		inv.x, inv.y, inv.z, inv.w);
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f     : Normalize",
+		normal.x, normal.y, normal.z, normal.w);
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f  : Multiply(q1,q2)",
+		mul1.x, mul1.y, mul1.z, mul1.w);
+	ImGui::Text("%1.2f %1.2f %1.2f %1.2f : Multiply(q2,q1)",
+		mul2.x, mul2.y, mul2.z, mul2.w);
+	ImGui::Text("%1.2f                    : Norm", norm);
+	ImGui::End();
 }
 
 void TitleScene::Draw() {
